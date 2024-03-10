1bc76d2ebae52ab6ed08497f3a706748d0c4d27c
parthvaghela <parthvaghela888@gmail.com>
2024-03-10 17:00:29 +0530

    new commit

---
+++ src/main/java/com/BidWheels/demo/ShippingWarsApplication.java
@@ -0,0 +1,13 @@
+package com.BidWheels.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingWarsApplication.class, args);
+	}
+
+}
